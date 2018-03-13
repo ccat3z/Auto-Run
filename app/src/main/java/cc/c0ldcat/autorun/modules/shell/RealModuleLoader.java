aa473931a25db51a9a0cfe345fb04d1cba72cd81
c0ldcat <firez199984@gmail.com>
2018-03-13 23:44:42 +0800

    update RealModuleLoader module
    获得realClassLoader后不再尝试获取

--- app/src/main/java/cc/c0ldcat/autorun/modules/shell/RealModuleLoader.java
+++ app/src/main/java/cc/c0ldcat/autorun/modules/shell/RealModuleLoader.java
@@ -2,7 +2,7 @@ package cc.c0ldcat.autorun.modules.shell;
 
 import cc.c0ldcat.autorun.BuildConfig;
 import cc.c0ldcat.autorun.modules.Module;
-import cc.c0ldcat.autorun.modules.real.Test;
+import cc.c0ldcat.autorun.modules.real.GetCheckPoint;
 import cc.c0ldcat.autorun.utils.LogUtils;
 import cc.c0ldcat.autorun.utils.RefectHelper;
 import de.robv.android.xposed.XC_MethodHook;
@@ -22,12 +22,18 @@ public class RealModuleLoader extends Module {
         XposedHelpers.findAndHookMethod("java.lang.ClassLoader", classLoader, "loadClass", String.class, boolean.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                if (realClassLoader != null)
+                    return;
+
                 Class<?> cls = (Class<?>) param.getResult();
+                if (cls == null)
+                    return;
+
                 Object dex = RefectHelper.getPrivateMethod(Class.forName("java.lang.Class"), "getDex").invoke(cls);
                 byte[] dexBytes = (byte[]) RefectHelper.getPrivateMethod(Class.forName("com.android.dex.Dex"), "getBytes").invoke(dex);
 
 
-                if (dexBytes.length == BuildConfig.REAL_DEX_LENGTH && realClassLoader == null) {
+                if (dexBytes.length == BuildConfig.REAL_DEX_LENGTH) {
                     realClassLoader = (ClassLoader) param.thisObject;
                     LogUtils.i("get real ClassLoader: " + classLoader.hashCode());
 
