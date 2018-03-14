5e37fc5b2284f1bca5344aead332086d4fdf55af
c0ldcat <firez199984@gmail.com>
2018-03-14 13:42:55 +0800

    update ReflectWrapper
    add Method

--- app/src/main/java/cc/c0ldcat/autorun/wrappers/ReflectWrapper.java
+++ app/src/main/java/cc/c0ldcat/autorun/wrappers/ReflectWrapper.java
@@ -1,8 +1,13 @@
 package cc.c0ldcat.autorun.wrappers;
 
+import cc.c0ldcat.autorun.utils.CommonUtils;
+import cc.c0ldcat.autorun.utils.LogUtils;
 import cc.c0ldcat.autorun.utils.ReflectHelper;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReflectWrapper {
     private Object object;
@@ -15,8 +20,39 @@ public class ReflectWrapper {
         return ReflectHelper.getPrivateObject(getObjectClass(), name, object);
     }
 
+    public Object getAttributeIfExist(String name) {
+        try {
+            return getAttribute(name);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            return null;
+        }
+    }
+
     public Method getMethod(String methodName, Class<?> ...params) throws NoSuchMethodException {
         return ReflectHelper.getPrivateMethod(getObjectClass(), methodName, params);
+    }
+
+    public Object invokeMethodIfAccessable(String methodName, Object ...params) {
+        try {
+            return invokeMethod(methodName, params);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            LogUtils.e(CommonUtils.exceptionStacktraceToString(e));
+            return null;
+        }
+    }
+
+    public Object invokeMethod(String methodName, Object ...params) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        List<Class<?>> paramsClassList = new ArrayList<>();
+
+        for (Object param : params) {
+            paramsClassList.add(param.getClass());
+        }
+
+        return invokeMethod(getMethod(methodName, paramsClassList.toArray(new Class[paramsClassList.size()])));
+    }
+
+    public Object invokeMethod(Method method, Object ...params) throws IllegalAccessException, InvocationTargetException {
+        return method.invoke(object, params);
     }
 
     public Class<?> getObjectClass() {
