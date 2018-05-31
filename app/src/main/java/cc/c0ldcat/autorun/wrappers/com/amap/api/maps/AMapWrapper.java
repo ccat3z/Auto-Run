aff88bac56f5e405a4d111a92374eca9f22dcd88
c0ldcat <firez199984@gmail.com>
2018-06-01 00:16:51 +0800

    update AMapWrapper: support OnMapClickListener

--- app/src/main/java/cc/c0ldcat/autorun/wrappers/com/amap/api/maps/AMapWrapper.java
+++ app/src/main/java/cc/c0ldcat/autorun/wrappers/com/amap/api/maps/AMapWrapper.java
@@ -1,10 +1,15 @@
 package cc.c0ldcat.autorun.wrappers.com.amap.api.maps;
 
 import cc.c0ldcat.autorun.models.Location;
+import cc.c0ldcat.autorun.utils.LogUtils;
 import cc.c0ldcat.autorun.wrappers.ReflectWrapper;
 import cc.c0ldcat.autorun.wrappers.com.amap.api.maps.model.LatLngWrapper;
 import cc.c0ldcat.autorun.wrappers.com.amap.api.maps.model.MarkerOptionsWrapper;
+import de.robv.android.xposed.XposedHelpers;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 
 public class AMapWrapper extends ReflectWrapper<AMapWrapper> {
@@ -30,5 +35,33 @@ public class AMapWrapper extends ReflectWrapper<AMapWrapper> {
 
     public Object addMarker(Location location) {
         return addMarker(location.getLongitude(), location.getLatitude());
+    }
+
+    public void setOnMapClickListener(final OnMapClickListener onMapClickListener) {
+        try {
+            Class<?> realOnMapCLickListenerInterface = XposedHelpers.findClass(getObjectClass().getName() + "$OnMapClickListener", getObjectClass().getClassLoader());
+            Class<?>[] interfaces = {realOnMapCLickListenerInterface};
+            invokeMethodIfAccessable("setOnMapClickListener", interfaces, Proxy.newProxyInstance(getObjectClass().getClassLoader(), interfaces, new InvocationHandler() {
+                @Override
+                public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+                    if (method.getName().equals("onMapClick")) {
+                        LatLngWrapper latLng = new LatLngWrapper();
+                        latLng.setObject(objects[0]);
+
+                        onMapClickListener.onMapClick(latLng);
+
+                        return null;
+                    }
+
+                    return null;
+                }
+            }));
+        } catch (XposedHelpers.ClassNotFoundError e) {
+            LogUtils.e(e);
+        }
+    }
+
+    public interface OnMapClickListener {
+        void onMapClick(LatLngWrapper point);
     }
 }
